fbfa37278740b566b24141ed5f23a429b4a7c011
Dmitry Zudochkin <dnd.pliz@gmail.com>
2013-04-29 23:27:01 +0400

    Flip Button shows all cards randomly

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,10 +7,14 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+
+@property (strong, nonatomic) PlayingCardDeck *deck;
 
 @end
 
@@ -25,6 +29,20 @@
 - (IBAction)flipCard:(UIButton *)sender
 {
     sender.selected = !sender.isSelected;
+    
+    if (sender.isSelected) {
+        Card *card = self.deck.drawRandomCard;
+        
+        [sender setTitle:card.contents forState:UIControlStateSelected];
+    }
+    
     self.flipCount++;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.deck = [[PlayingCardDeck alloc] init];
 }
 @end
