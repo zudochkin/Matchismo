650dc625e6c9104d6e72031c0479e324791ad20b
Dmitry Zudochkin <dnd.pliz@gmail.com>
2013-04-29 23:04:36 +0400

    Added some models (Card, Deck, PlayingCard, PlayingDeck)

---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Dmitry Zudochkin on 4/29/13.
+//  Copyright (c) 2013 Dmitry Zudochkin. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
